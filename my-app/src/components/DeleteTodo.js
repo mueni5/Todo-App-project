b3eb15097f6a442b20b53df71f47767217ba633c
+ <+>
2023-05-05 00:03:39 +0300

    In progress

--- my-app/src/components/DeleteTodo.js
+++ my-app/src/components/DeleteTodo.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const DeleteTodo = ({ id, refreshTodos }) => {
   const deleteTodo = async () => {
     try {
-      await axios.delete(`http://localhost:3000/todos/${id}`);
+      await axios.delete(`http://localhost:3000/tasks/${id}`);
       refreshTodos();
     } catch (error) {
       console.log(error);
